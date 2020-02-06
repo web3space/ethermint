5ca43f95accad59dda034654ed9c0179b3b764c9
Oleksandr Puzanov <oleksandrpuzanov@MacBook-Pro-Oleksandr.local>
2020-02-06 23:38:50 +0200

    fix transactions

--- x/evm/types/codec.go
+++ x/evm/types/codec.go
@@ -5,18 +5,20 @@ import (
 )
 
 // ModuleCdc defines the codec to be used by evm module
-var ModuleCdc = codec.New()
+var ModuleCdc *codec.Codec// = codec.New()
 
-func init() {
-	cdc := codec.New()
-
-	codec.RegisterCrypto(cdc)
-
-	ModuleCdc = cdc.Seal()
-}
+//func init() {
+//	cdc := codec.New()
+//
+//	codec.RegisterCrypto(cdc)
+//
+//	ModuleCdc = cdc.Seal()
+//}
 
 // RegisterCodec registers concrete types and interfaces on the given codec.
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(&EthereumTxMsg{}, "ethermint/MsgEthereumTx", nil)
 	cdc.RegisterConcrete(&EmintMsg{}, "ethermint/MsgEmint", nil)
+
+	ModuleCdc = cdc
 }
